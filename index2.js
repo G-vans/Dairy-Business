f0f2d0ea3356d8d2ddee119330335ecd3fa5b257
G-vans <jevans.otieno@student.moringaschool.com>
2022-07-18 08:49:12 +0300

    update multiply function

---
+++ index2.js
@@ -0,0 +1,29 @@
+ //Sales- weekly
+    
+    
+ var x = multiply(3100, 7, 45)
+ document.getElementById("arrSales").innerHTML = x;
+ function multiply(a, b, c)
+ {
+ return (a * b *c)
+ }
+
+ //Sales- monthly
+    
+    
+ var x = multiply(3100, 30, 45)
+ document.getElementById("arrSales2").innerHTML = x;
+ function multiply(a, b, c)
+ {
+ return (a * b *c)
+ }
+
+ //Sales- Yearly
+    
+    
+ var x = multiply(3100, 365, 45)
+ document.getElementById("arrSales3").innerHTML = x;
+ function multiply(a, b, c)
+ {
+ return (a * b *c)
+ }
