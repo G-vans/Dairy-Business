bd9212a92eab91351f486fc79561d01731101958
G-vans <jevans.otieno@student.moringaschool.com>
2022-07-18 05:21:22 +0300

    Add sales function

--- index.js
+++ index.js
@@ -40,3 +40,13 @@ function buildTable(data) {
 //Total Production
 let arr = ["Total Production: 3,100 Litres per day"];
     document.getElementById("arrPrint").innerHTML = arr;
+
+    //Sales
+    var sales = [3,100];
+    var scalar = 45;
+    
+    sales.forEach((value, index) => {
+        sales[index] *= scalar;
+    });
+    console.log(sales);
+    document.getElementById("arrSales").innerHTML = sales;
