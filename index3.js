856a63ace67c4a9badc9ee7345c1e5627bfc3bc7
G-vans <jevans.otieno@student.moringaschool.com>
2022-07-18 08:49:24 +0300

    Add new function

---
+++ index3.js
@@ -0,0 +1,116 @@
+//Sales- weekly
+    
+    
+var x = multiply(3100, 31, 45)
+document.getElementById("arrSales").innerHTML = x;
+function multiply(a, b, c)
+{
+return (a * b *c)
+}
+
+//Sales- monthly
+   
+   
+var x = multiply(3100, 28, 45)
+document.getElementById("arrSales2").innerHTML = x;
+function multiply(a, b, c)
+{
+return (a * b *c)
+}
+
+//Sales- Yearly
+   
+   
+var x = multiply(3100, 31, 45)
+document.getElementById("arrSales3").innerHTML = x;
+function multiply(a, b, c)
+{
+return (a * b *c)
+}
+//Sales- weekly
+    
+    
+var x = multiply(3100, 30, 45)
+document.getElementById("arrSales4").innerHTML = x;
+function multiply(a, b, c)
+{
+return (a * b *c)
+}
+
+//Sales- monthly
+   
+   
+var x = multiply(3100, 31, 45)
+document.getElementById("arrSales5").innerHTML = x;
+function multiply(a, b, c)
+{
+return (a * b *c)
+}
+
+//Sales- Yearly
+   
+   
+var x = multiply(3100, 30, 45)
+document.getElementById("arrSales6").innerHTML = x;
+function multiply(a, b, c)
+{
+return (a * b *c)
+}
+//Sales- weekly
+    
+    
+var x = multiply(3100, 31, 45)
+document.getElementById("arrSales7").innerHTML = x;
+function multiply(a, b, c)
+{
+return (a * b *c)
+}
+
+//Sales- monthly
+   
+   
+var x = multiply(3100, 31, 45)
+document.getElementById("arrSales8").innerHTML = x;
+function multiply(a, b, c)
+{
+return (a * b *c)
+}
+
+//Sales- Yearly
+   
+   
+var x = multiply(3100, 30, 45)
+document.getElementById("arrSales9").innerHTML = x;
+function multiply(a, b, c)
+{
+return (a * b *c)
+}
+//Sales- weekly
+    
+    
+var x = multiply(3100, 31, 45)
+document.getElementById("arrSales10").innerHTML = x;
+function multiply(a, b, c)
+{
+return (a * b *c)
+}
+
+//Sales- monthly
+   
+   
+var x = multiply(3100, 30, 45)
+document.getElementById("arrSales11").innerHTML = x;
+function multiply(a, b, c)
+{
+return (a * b *c)
+}
+
+//Sales- Yearly
+   
+   
+var x = multiply(3100, 31, 45)
+document.getElementById("arrSales12").innerHTML = x;
+function multiply(a, b, c)
+{
+return (a * b *c)
+}
